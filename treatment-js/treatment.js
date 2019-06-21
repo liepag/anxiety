78ee7cb3247783a3ead5471348408ea4423e7f76
Liepa <liepagrigaite@gmail.com>
2019-06-21 13:14:42 +0100

    text appearing/disappearing on click

--- treatment-js/treatment.js
+++ treatment-js/treatment.js
@@ -1,5 +1,8 @@
-$(document).ready(function () {
-  $( "custom-rounded" ).click(function () {
-    $("text").toggle();
-  });
+$('.img__wrap-therapy img').on('click', function()  {
+    $('.img__description-therapy').toggle();
+    // e.preventDefault();
+});
+$('.img__wrap-medication img').on('click', function()  {
+    $('.img__description-medication').toggle();
+    // e.preventDefault();
 });
